8ce960eec7afec88cdb1c2895443e069c5fff78d
Rodolfo Vieira <rodolfovieira95@outlook.com>
2024-08-27 00:37:15 -0300

    feat: adds artists mock

---
+++ src/utils/artists.ts
@@ -0,0 +1,365 @@
+export const artists = [
+  {
+    id: 1,
+    name: "Jonas Aden",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 2,
+    name: "Alpharock",
+    img: "https://assets.amsterdam-dance-event.nl/images/images/artists-speakers/_900xAUTO_crop_center-center_none/Alpharock_82889.jpg",
+  },
+  {
+    id: 3,
+    name: "Area21",
+    img: "https://cdns-images.dzcdn.net/images/artist/26b20d5eecb0580d4dc306d168cd6cdc/500x500.jpg",
+  },
+  {
+    id: 4,
+    name: "Arty",
+    img: "https://cdns-images.dzcdn.net/images/artist/0447b01e04770f15ca0983f117d5f73b/1900x1900-000000-80-0-0.jpg",
+  },
+  {
+    id: 5,
+    name: "Aspyer",
+    img: "https://dancingastronaut.com/wp-content/uploads/2021/12/Screen-Shot-2021-12-26-at-8.21.46-PM.jpg",
+  },
+  {
+    id: 6,
+    name: "Bad Decisions",
+    img: "https://scontent.fcgh15-1.fna.fbcdn.net/v/t1.6435-9/43737376_2220606374821611_4811556955816460288_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=13d280&_nc_eui2=AeGCC24vMTRzW9BH4mdkwIblQ0WF6No_6rpDRYXo2j_qugMW0_V1iq0Xld7G4YXefuy4PBc4xutHxhSb6xqdaKFx&_nc_ohc=vE0H7P09hB0Q7kNvgFU7F-V&_nc_ht=scontent.fcgh15-1.fna&oh=00_AYCuAXQmWkFKPAPoPbtqEfP7tynptDEBEHqkNLBcFGqNPA&oe=66EB8930",
+  },
+  {
+    id: 7,
+    name: "Badflite",
+    img: "https://edmjoy.com/wp-content/uploads/2018/11/BADFLITE-Nov-2018-FI.jpg",
+  },
+  {
+    id: 8,
+    name: "Taska Black",
+    img: "https://i0.wp.com/lefuturewave.com/wp-content/uploads/2021/10/ETZUR9jg-1-e1633817648438.png?resize=950%2C534&ssl=1",
+  },
+  {
+    id: 9,
+    name: "Bleu Clair",
+    img: "https://d3vhc53cl8e8km.cloudfront.net/hello-staging/wp-content/uploads/2021/06/13175821/b1fa93b8-f1b7-11ed-954e-0ecc81f4ee58-972x597.jpg",
+  },
+  {
+    id: 10,
+    name: "Blinders",
+    img: "https://static.wikia.nocookie.net/edm/images/2/28/Blinders.jpg/revision/latest?cb=20210831033635",
+  },
+  {
+    id: 11,
+    name: "Brooks",
+    img: "https://images.squarespace-cdn.com/content/v1/5512c9aae4b0b2a9ab91ab0a/1590479640345-U87F054J89TYCWZC4UPF/Brooks3B.png",
+  },
+  {
+    id: 12,
+    name: "Caius",
+    img: "",
+  },
+  {
+    id: 13,
+    name: "Cazztek",
+    img: "https://geo-media.beatport.com/image_size/590x404/39d911eb-6f6a-47cc-abd6-5a0334f74903.jpg",
+  },
+  {
+    id: 14,
+    name: "Cesqeaux",
+    img: "https://upload.wikimedia.org/wikipedia/commons/0/04/Cesqeaux.jpg",
+  },
+  {
+    id: 15,
+    name: "CLiQ",
+    img: "https://i1.sndcdn.com/avatars-000495668562-orixdr-t1080x1080.jpg",
+  },
+  {
+    id: 16,
+    name: "CMC$",
+    img: "https://i.scdn.co/image/ab6761610000e5eb40b47978985b82bbf85c9bcd",
+  },
+  {
+    id: 17,
+    name: "Codes",
+    img: "",
+  },
+  {
+    id: 18,
+    name: "Syn Cole",
+    img: "https://yt3.googleusercontent.com/ytc/AIdro_n1re1lcPVIH74pYUYU8gnJ955LJMv83atJtst5r8tYTLM=s900-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: 19,
+    name: "Conro",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 20,
+    name: "Crash Land",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 21,
+    name: "Disero",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 22,
+    name: "DubVision",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 23,
+    name: "Dyro",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 24,
+    name: "Dzeko",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 25,
+    name: "Eauxmar",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 26,
+    name: "Ralph Felix",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 27,
+    name: "Focus Fire",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 28,
+    name: "Dillon Francis",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 29,
+    name: "Pierce Fulton",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 30,
+    name: "Salvatore Ganacci",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 31,
+    name: "Martin Garrix",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 32,
+    name: "Goja",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 33,
+    name: "GRX",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 34,
+    name: "Jay Hardway",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 35,
+    name: "Hawksburn",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 36,
+    name: "Todd Helder",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 37,
+    name: "Seth Hills",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 38,
+    name: "Ibranovski",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 39,
+    name: "Infuze",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 40,
+    name: "Julian Jordan",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 41,
+    name: "Kev",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 42,
+    name: "King Arthur",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 43,
+    name: "Lione",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 44,
+    name: "Lodgerz",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 45,
+    name: "Lost Frequencies",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 46,
+    name: "Loopers",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 47,
+    name: "Magnificence",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 48,
+    name: "John Martin",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 49,
+    name: "Tom Martin",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 50,
+    name: "Matisse & Sadko",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 51,
+    name: "Mesto",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 52,
+    name: "Moksi",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 53,
+    name: "Bart B More",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 54,
+    name: "Justin Mylo",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 55,
+    name: "Osrin",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 56,
+    name: "Florian Picasso",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 57,
+    name: "Perk Pietrek",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 58,
+    name: "Raiden",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 59,
+    name: "Junior Sanchez",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 60,
+    name: "Silque",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 61,
+    name: "Slvr",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 62,
+    name: "Third Party",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 63,
+    name: "TV Noise",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 64,
+    name: "Two Pauz",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 65,
+    name: "Van Duo",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 66,
+    name: "Michel Zitron",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 67,
+    name: "Zonderling",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+];
+
+export const userCollectedStickerts = [
+  {
+    id: 15,
+    name: "CLiQ",
+    img: "https://i1.sndcdn.com/avatars-000495668562-orixdr-t1080x1080.jpg",
+  },
+  {
+    id: 1,
+    name: "Jonas Aden",
+    img: "https://i.scdn.co/image/ab6761610000e5eb3a4c7838d9f3e0f07e55023c",
+  },
+  {
+    id: 2,
+    name: "Alpharock",
+    img: "https://assets.amsterdam-dance-event.nl/images/images/artists-speakers/_900xAUTO_crop_center-center_none/Alpharock_82889.jpg",
+  },
+  {
+    id: 7,
+    name: "Badflite",
+    img: "https://edmjoy.com/wp-content/uploads/2018/11/BADFLITE-Nov-2018-FI.jpg",
+  },
+  {
+    id: 11,
+    name: "Brooks",
+    img: "https://images.squarespace-cdn.com/content/v1/5512c9aae4b0b2a9ab91ab0a/1590479640345-U87F054J89TYCWZC4UPF/Brooks3B.png",
+  },
+];
