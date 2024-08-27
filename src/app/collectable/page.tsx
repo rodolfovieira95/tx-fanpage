f317251e2793256b67646ee7981ed20e332ac202
Rodolfo Vieira <rodolfovieira95@outlook.com>
2024-08-27 00:40:41 -0300

    feat: updates collectable page

--- src/app/collectable/page.tsx
+++ src/app/collectable/page.tsx
@@ -1,15 +1,19 @@
 import Postcard from "@/components/Postcard";
-import { data } from "@/utils/stamps";
+import { data, userCollectedStickerts } from "@/utils/stamps";
 
 export default function Collectable() {
   return (
     <main className="flex flex-col px-20 h-full">
       <h1 className="text-white text-4xl">Collectables</h1>
       <div className="flex flex-wrap mt-8 gap-4 justify-between">
-        {data.map(({ stamp, date, eventName }) => {
+        {data.map(({ stamp, date, eventName, id }) => {
           return (
             <div key={`${eventName}-${date}`} className="max-w-[180px]">
-              <Postcard img={stamp} />
+              <Postcard
+                id={id}
+                img={stamp}
+                userCollectedStickerts={userCollectedStickerts}
+              />
               <h1 className="font-bold px-1">{eventName}</h1>
               <p className="font-thin px-1">{date}</p>
             </div>
